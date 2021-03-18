eb912b8f8cf6ed9528880db7d522d51340face0b
Angela George <ageorge5@zagmail.gonzaga.edu>
2021-03-18 17:25:04 -0500

    log in button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import { useAuth0 } from '@auth0/auth0-react';
+import Spinner from 'react-bootstrap/Spinner'
 
 function App() {
+  const {isLoading} = useAuth0();
+  if (isLoading) return (
+    
+    <Spinner animation="border" role="status">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
+    )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div>   
+      <LoginButton/>
+      <p>home page</p>
     </div>
   );
 }
